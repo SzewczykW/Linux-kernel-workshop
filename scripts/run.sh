a3ce3318f3a4a87d630d9a9a7e217ff87e13b3c8
SzewczykW <wiktorsz2002@gmail.com>
2024-10-12 17:40:09 +0200

    Add run script with qemu

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+qemu-system-x86_64 \
+  -kernel ../linux/arch/x86_64/boot/bzImage \
+  -nographic \
+  -drive format=raw,file=../buildroot/output/images/rootfs.ext4,if=virtio \
+  -append "root=/dev/vda console=ttyS0 nokaslr other-paras-here-if-needed" \
+  -m 4G \
+  -enable-kvm \
+  -cpu host \
+  -smp $(nproc) \
+  -net nic,model=virtio \
+  -net user,hostfwd=tcp::10022-:22
